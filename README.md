112d5169dbbbedc7f3a961384b799c5beef48d0b
marcospss <marcospss@gmail.com>
2020-10-14 16:57:40 -0300

    setup project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Webpack React Setup
+
+## Install
+
+To install the dependencies
+
+```
+$ npm i
+```
+
+To run the application
+
+```
+$ npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack').container.ModuleFederationPlugin;
+const path = require('path');
+
+module.exports = {
+  entry: './src/index',
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+  },
+  output: {
+    publicPath: 'http://localhost:3000/',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /bootstrap\.tsx$/,
+        loader: 'bundle-loader',
+        options: {
+          lazy: true,
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-typescript'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'app1',
+      remotes: {},
+      shared: ['react', 'react-dom'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
