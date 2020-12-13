dd2ed18ae9d9a50c842b2880d52a38cfb04cd8df
marcospss <marcospss@gmail.com>
2020-12-13 20:05:46 -0300

    New configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack').container.ModuleFederationPlugin;
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const HotModuleReplacementPlugin = require('webpack').HotModuleReplacementPlugin;
+const Dotenv = require('dotenv-webpack');
 const path = require('path');
+// https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin
+// const { GenerateSW } = require('workbox-webpack-plugin');
+
+const dependencies = require('./package.json').dependencies;
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: './src/index',
-  mode: 'development',
+  mode: isDevelopment ? 'development' : 'production',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     port: 3000,
@@ -26,23 +35,40 @@ module.exports = {
         },
       },
       {
-        test: /\.tsx?$/,
+        test: /\.[jt]sx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
           presets: ['@babel/preset-react', '@babel/preset-typescript'],
+          plugins: ['@babel/plugin-transform-runtime', isDevelopment && require.resolve('react-refresh/babel')],
         },
       },
     ],
   },
   plugins: [
+    isDevelopment && new HotModuleReplacementPlugin(),
+    isDevelopment && new ReactRefreshPlugin(),
+    isDevelopment && new Dotenv(),
     new ModuleFederationPlugin({
-      name: 'app1',
+      name: 'container',
       remotes: {},
-      shared: ['react', 'react-dom'],
+      shared: [
+        {
+          ...dependencies,
+          react: {
+            singleton: true,
+            requiredVersion: dependencies.react,
+          },
+          'react-dom': {
+            singleton: true,
+            requiredVersion: dependencies['react-dom'],
+          },
+        },
+      ],
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
+    // new GenerateSW(), TODO: Config PWA
   ],
 };
