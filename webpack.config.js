3922f305f1857109627b8880eea28305d57f783a
marcospss <marcospss@gmail.com>
2020-12-17 11:20:13 -0300

    feat(webpack): webpack config Fast Refresh

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack').container.ModuleFederationPlugin;
-const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const HotModuleReplacementPlugin = require('webpack').HotModuleReplacementPlugin;
 const Dotenv = require('dotenv-webpack');
 const path = require('path');
@@ -12,63 +12,63 @@ const dependencies = require('./package.json').dependencies;
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  entry: './src/index',
-  mode: isDevelopment ? 'development' : 'production',
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    port: 3000,
-    historyApiFallback: true,
-  },
-  output: {
-    publicPath: 'http://localhost:3000/',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /bootstrap\.tsx$/,
-        loader: 'bundle-loader',
-        options: {
-          lazy: true,
-        },
-      },
-      {
-        test: /\.[jt]sx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: ['@babel/preset-react', '@babel/preset-typescript'],
-          plugins: ['@babel/plugin-transform-runtime', isDevelopment && require.resolve('react-refresh/babel')],
-        },
-      },
-    ],
-  },
-  plugins: [
-    isDevelopment && new HotModuleReplacementPlugin(),
-    isDevelopment && new ReactRefreshPlugin(),
-    isDevelopment && new Dotenv(),
-    new ModuleFederationPlugin({
-      name: 'container',
-      remotes: {},
-      shared: [
-        {
-          ...dependencies,
-          react: {
-            singleton: true,
-            requiredVersion: dependencies.react,
-          },
-          'react-dom': {
-            singleton: true,
-            requiredVersion: dependencies['react-dom'],
-          },
-        },
-      ],
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    // new GenerateSW(), TODO: Config PWA
-  ],
+    entry: './src/index',
+    mode: isDevelopment ? 'development' : 'production',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 3000,
+        historyApiFallback: true,
+    },
+    output: {
+        publicPath: 'http://localhost:3000/',
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /bootstrap\.tsx$/,
+                loader: 'bundle-loader',
+                options: {
+                    lazy: true,
+                },
+            },
+            {
+                test: /\.[jt]sx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: ['@babel/preset-react', '@babel/preset-typescript'],
+                    plugins: ['@babel/plugin-transform-runtime', isDevelopment && require.resolve('react-refresh/babel')],
+                },
+            },
+        ].filter(Boolean),
+    },
+    plugins: [
+        isDevelopment && new HotModuleReplacementPlugin(),
+        isDevelopment && new ReactRefreshWebpackPlugin(),
+        isDevelopment && new Dotenv(),
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {},
+            shared: [
+                {
+                    ...dependencies,
+                    react: {
+                        singleton: true,
+                        requiredVersion: dependencies.react,
+                    },
+                    'react-dom': {
+                        singleton: true,
+                        requiredVersion: dependencies['react-dom'],
+                    },
+                },
+            ],
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+        // new GenerateSW(), TODO: Config PWA
+    ].filter(Boolean),
 };
